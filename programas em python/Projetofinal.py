65ce21330e0cbc7dce42ec6f9fa419547e7d0975
Miguel <vianamiguelangep@gmail.com>
2026-01-30 23:27:48 -0300

    iniciando o projeto final

---
+++ programas em python/Projetofinal.py
@@ -0,0 +1,10 @@
+#ultima aula de Dados com pytho. Construindo um dashboard interativo!
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+print(df.head())
+
+
